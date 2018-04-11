b3334b92a87f57c07db72e10860fb273335a3d07
pengmin <pengmin_bj@lakala.com>
2018-04-11 09:37:13 +0800

    登录页面，初始化view控件

--- app/src/main/java/com/yunhui/activity/LoginActivity.java
+++ app/src/main/java/com/yunhui/activity/LoginActivity.java
@@ -1,16 +1,54 @@
 package com.yunhui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.yunhui.R;
+import com.yunhui.component.edittext.ClearEditTextLogin;
+import com.yunhui.component.edittext.ShowPasswordEditText;
 
 /**
  * Created by pengmin on 2018/4/9.
  * 登录页面
  */
 
-public class LoginActivity extends BaseActionBarActivity {
+public class LoginActivity extends BaseActionBarActivity implements View.OnClickListener{
+
+    private ClearEditTextLogin clearEditTextLogin;
+    private ShowPasswordEditText showPasswordEditText;
+    private Button btn_LoginEnter;
+    private Button btn_LoginRegist;
+    private Button btn_loginForgetPassword;
 
     @Override
     protected void initActivity(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_login);
+        initView();
+    }
 
+    /**
+     * 初始话view
+     */
+    private void initView(){
+        clearEditTextLogin = findViewById(R.id.login_phonenum);
+        showPasswordEditText = findViewById(R.id.login_password);
+        btn_LoginEnter = findViewById(R.id.login_enter);
+        btn_LoginRegist = findViewById(R.id.login_regist);
+        btn_loginForgetPassword = findViewById(R.id.login_forgetpassword);
+    }
+
+    @Override
+    public void onClick(View view) {
+        super.onClick(view);
+
+        switch (view.getId()){
+            case R.id.login_enter://登录
+                break;
+            case R.id.login_regist://注册
+                break;
+            case R.id.login_forgetpassword://忘记密码
+                break;
+        }
     }
 }
