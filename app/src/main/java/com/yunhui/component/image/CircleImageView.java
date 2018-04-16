4494408ef03bf484afa452d94c29edd20241e3bd
pengmin <pengmin_bj@lakala.com>
2018-04-16 13:53:31 +0800

    我的页面增加布局

---
+++ app/src/main/java/com/yunhui/component/image/CircleImageView.java
@@ -0,0 +1,83 @@
+package com.yunhui.component.image;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by pengmin on 2018/4/16.
+ * 圆型的imageView
+ */
+
+public class CircleImageView extends ImageView{
+
+    private BitmapShader bitmapShader = null;
+    private Bitmap bitmap = null;
+    private ShapeDrawable shapeDrawable = null;
+    private int width = 0;
+    private int height = 0;
+
+
+    public CircleImageView(Context context) {
+        super(context);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public CircleImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        try {
+            width = getWidth();
+            height = getHeight();
+
+
+            if (getDrawable() == null) return;
+
+            bitmap = ((BitmapDrawable) getDrawable()).getBitmap();
+
+            if (bitmap == null) return;
+
+            if (width <= 0 || height <= 0) return;
+            // 重新生成指定大小的图片
+            bitmap = Bitmap.createScaledBitmap(bitmap, width, height,
+                    true);
+
+            if (bitmap == null) return;
+
+            // 构造渲染器BitmapShader
+            bitmapShader = new BitmapShader(bitmap, Shader.TileMode.MIRROR,
+                    Shader.TileMode.REPEAT);
+
+            // 将图片裁剪为椭圆形
+            // 构建ShapeDrawable对象并定义形状为椭圆
+            shapeDrawable = new ShapeDrawable(new OvalShape());
+            // 得到画笔并设置渲染器
+            shapeDrawable.getPaint().setShader(bitmapShader);
+            // 设置显示区域
+            shapeDrawable.setBounds(0, 0, width, height);
+            // 绘制shapeDrawable
+            shapeDrawable.draw(canvas);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
--- app/src/main/java/com/yunhui/fragmenr/MyFragment.java
+++ app/src/main/java/com/yunhui/fragmenr/MyFragment.java
@@ -5,9 +5,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.yunhui.activity.HomeActivity;
 import com.yunhui.R;
+import com.yunhui.component.image.CircleImageView;
+import com.yunhui.component.linearlayout.LabelItemView;
 
 /**
  * Created by pengmin on 2018/4/2.
@@ -18,12 +21,55 @@ public class MyFragment extends BaseFragment {
 
     private HomeActivity homeActivity;
     private View parentView;
+    private CircleImageView civ_userPhoto;
+    private TextView tv_userPhoneNum;
+    private TextView tv_userName;
+    private LabelItemView liv_exchange;
+    private LabelItemView liv_myTask;
+    private LabelItemView liv_buy;
+    private LabelItemView liv_myInvitation;
+    private LabelItemView liv_aboutMy;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         homeActivity = (HomeActivity) getActivity();
         parentView = inflater.inflate(R.layout.fragment_my,null);
+        initView();
         return parentView;
+    }
+
+    private void initView() {
+        civ_userPhoto = parentView.findViewById(R.id.userphoto);
+        tv_userPhoneNum = parentView.findViewById(R.id.userphonenum);
+        tv_userName = parentView.findViewById(R.id.username);
+        liv_exchange = parentView.findViewById(R.id.exchange);
+        liv_myTask = parentView.findViewById(R.id.myTask);
+        liv_buy = parentView.findViewById(R.id.buy);
+        liv_myInvitation = parentView.findViewById(R.id.myInvitation);
+        liv_aboutMy = parentView.findViewById(R.id.aboutMy);
+        liv_exchange.setOnClickListener(this);
+        liv_myTask.setOnClickListener(this);
+        liv_buy.setOnClickListener(this);
+        liv_myInvitation.setOnClickListener(this);
+        liv_aboutMy.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        super.onClick(view);
+        switch (view.getId()){
+            case R.id.exchange:
+                break;
+            case R.id.myTask:
+                break;
+            case R.id.buy:
+                break;
+            case R.id.myInvitation:
+                break;
+            case R.id.aboutMy:
+                break;
+        }
     }
 }
