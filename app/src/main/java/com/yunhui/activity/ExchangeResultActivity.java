11681d6099eaaf267cfaa5df10cc0e4ee6d63310
pengmin <pengmin_bj@lakala.com>
2018-05-01 10:16:38 +0800

    增加兑换结果页面

---
+++ app/src/main/java/com/yunhui/activity/ExchangeResultActivity.java
@@ -0,0 +1,16 @@
+package com.yunhui.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by pengmin on 2018/5/1.
+ * 兑换页面
+ */
+
+public class ExchangeResultActivity extends BaseActionBarActivity{
+
+    @Override
+    protected void initActivity(Bundle savedInstanceState) {
+
+    }
+}
