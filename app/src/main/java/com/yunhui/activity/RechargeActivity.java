3d67b2d8953d1cfb25ae3437593cba5a61e2c6e1
pengmin <pengmin_bj@lakala.com>
2018-05-29 11:04:26 +0800

    云钻充值增加组合测试支付方式

--- app/src/main/java/com/yunhui/activity/RechargeActivity.java
+++ app/src/main/java/com/yunhui/activity/RechargeActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 
 import com.loopj.common.exception.BaseException;
 import com.loopj.common.httpEx.HttpRequest;
+import com.loopj.common.httpEx.HttpRequestParams;
 import com.loopj.common.httpEx.IHttpRequestEvents;
 import com.yunhui.R;
 import com.yunhui.YhApplication;
@@ -22,14 +24,18 @@ import com.yunhui.clickinterface.ListItemClickHelp;
 import com.yunhui.component.image.CircleImageView;
 import com.yunhui.component.refreshlistview.RefreshListView;
 import com.yunhui.manager.ActivityQueueManager;
+import com.yunhui.pay.AlRunnable;
+import com.yunhui.pay.PayResult;
 import com.yunhui.request.BuyRequestFactory;
 import com.yunhui.request.RequestUtil;
 import com.yunhui.util.ToastUtil;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 充值页面
@@ -50,6 +56,8 @@ public class RechargeActivity extends BaseActionBarActivity implements RefreshLi
     private int c2 = 0;
     private int c3 = 0;
     private double allTotal = 0.00;
+    private String billId;
+    private String signData;
 
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler(){
@@ -59,6 +67,23 @@ public class RechargeActivity extends BaseActionBarActivity implements RefreshLi
             switch (msg.what){
                 case 0000:
                     rechargeAdapter.refreshData(rechargeBeans);
+                    break;
+                case AlRunnable.SDK_PAY_FLAG:
+                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                    /**
+                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为9000则代表支付成功
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+//                        Toast.makeText(BuyillMInfoActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+                        RechargeActivity.this.finish();
+                    } else {
+                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                        Toast.makeText(RechargeActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+                    }
                     break;
             }
         }
@@ -90,7 +115,11 @@ public class RechargeActivity extends BaseActionBarActivity implements RefreshLi
         b_rechargepay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createPayBill();
+                if(c1 == 0 && c2 == 0 && c3 == 0){
+                    ToastUtil.toast(RechargeActivity.this,"请选择充值金额");
+                }else {
+                    createPayBill();
+                }
             }
         });
 
@@ -248,7 +277,92 @@ public class RechargeActivity extends BaseActionBarActivity implements RefreshLi
     }
 
     private void createPayBill(){
+        RequestUtil requestUtil = RequestUtil.obtainRequest(RechargeActivity.this,"user/yhPrePay", HttpRequest.RequestMethod.POST);
+        HttpRequestParams requestParams = requestUtil.getRequestParams();
+        requestParams.put("businessid","CH");
+        requestParams.put("amount",String.format("%.2f",allTotal));
+        JSONArray jsonArray = new JSONArray();
+        if(c1 != 0){
+            JSONObject jsonObjectC1= new JSONObject();
+            try {
+                jsonObjectC1.put("id","c1");
+                jsonObjectC1.put("num",String.valueOf(c1));
+                jsonArray.put(jsonObjectC1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
+        }
+        if(c2 != 0){
+            JSONObject jsonObjectC2= new JSONObject();
+            try {
+                jsonObjectC2.put("id","c2");
+                jsonObjectC2.put("num",String.valueOf(c2));
+                jsonArray.put(jsonObjectC2);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if(c3 != 0){
+            JSONObject jsonObjectC3= new JSONObject();
+            try {
+                jsonObjectC3.put("id","c3");
+                jsonObjectC3.put("num",String.valueOf(c3));
+                jsonArray.put(jsonObjectC3);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        requestParams.put("list",jsonArray);
+
+        requestUtil.setIHttpRequestEvents(new IHttpRequestEvents(){
+            @Override
+            public void onSuccess(HttpRequest request) {
+                super.onSuccess(request);
+                JSONObject jsonObject = (JSONObject) request.getResponseHandler().getResultData();
+                billId = jsonObject.optString("billId");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        alPay();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(HttpRequest request, BaseException exception) {
+                super.onFailure(request, exception);
+                Toast.makeText(RechargeActivity.this,"创建支付订单失败，请重试",Toast.LENGTH_SHORT);
+            }
+        });
+        requestUtil.execute();
+    }
+
+    private void alPay(){
+        RequestUtil requestUtil = BuyRequestFactory.doAlPay(RechargeActivity.this,billId);
+        requestUtil.setIHttpRequestEvents(new IHttpRequestEvents(){
+            @Override
+            public void onSuccess(HttpRequest request) {
+                super.onSuccess(request);
+                JSONObject jsonObject = (JSONObject) request.getResponseHandler().getResultData();
+                signData = jsonObject.optString("signData");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AlRunnable alRunnable = new AlRunnable(RechargeActivity.this,handler,signData);
+                        Thread payThread = new Thread(alRunnable);
+                        payThread.start();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(HttpRequest request, BaseException exception) {
+                super.onFailure(request, exception);
+                Toast.makeText(RechargeActivity.this,"创建支付订单失败，请重试",Toast.LENGTH_SHORT);
+            }
+        });
+        requestUtil.execute();
     }
 
     @Override
